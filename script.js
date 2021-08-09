e32bdf8ee6822b72a4681a54f4aabf053ba45c15
Roman Voronin <r.voronin@mobidev.biz>
2021-08-09 13:55:40 +0300

    2 gamefields with independent controls

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 class Game {
-    constructor() {
-        this.canvas = document.querySelector('.gamefield');
+    constructor(gamefield) {
+        this.canvas = document.querySelector(gamefield);
         this.context = this.canvas.getContext('2d');
-
+        this.gamefield = gamefield;
         this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.blockSize = 20;
@@ -25,14 +25,7 @@ class Game {
             tailLength: 3
         };
 
-        this.secondSnake = {
-            x: this.width / 4,
-            y: this.height / 4,
-            vx: this.blockSize,
-            vy: 0,
-            tail: [],
-            tailLength: 3
-        };
+
 
         this.food = {
             x: this.randomizer (2, this.blockWidth - 1) * this.blockSize,
@@ -49,100 +42,97 @@ class Game {
         this.frames = 0;
         this.context.clearRect(0, 0, this.width, this.height);
         this.control();
-        this.controlSecondSnake();
         this.drawScore();
         this.drawWalls();
         this.spawnFood();
         this.drawSnake(this.snake);
-        this.drawSnake(this.secondSnake);
         
     } 
 
     control() {
         document.addEventListener('keydown', (event) => {
-            if (event.which === 37 && this.snake.vx === 0) { // left
-                this.snake.vx = -this.blockSize;
-                this.snake.vy = 0;
-            } else if (event.which === 38 && this.snake.vy === 0) { // up
-                this.snake.vx = 0;
-                this.snake.vy = -this.blockSize;
-            } else if (event.which === 39 && this.snake.vx === 0) { // right
-                this.snake.vx = this.blockSize;
-                this.snake.vy = 0;
-            } else if (event.which === 40 && this.snake.vy === 0) { // down
-                this.snake.vx = 0;
-                this.snake.vy = this.blockSize;
+            if (this.gamefield === '.gamefield') {
+                if (event.which === 37 && this.snake.vx === 0) { // left
+                    this.snake.vx = -this.blockSize;
+                    this.snake.vy = 0;
+                } else if (event.which === 38 && this.snake.vy === 0) { // up
+                    this.snake.vx = 0;
+                    this.snake.vy = -this.blockSize;
+                } else if (event.which === 39 && this.snake.vx === 0) { // right
+                    this.snake.vx = this.blockSize;
+                    this.snake.vy = 0;
+                } else if (event.which === 40 && this.snake.vy === 0) { // down
+                    this.snake.vx = 0;
+                    this.snake.vy = this.blockSize;
+                }
+            } else if (this.gamefield === '.anotherGamefield') {
+                if (event.which === 65 && this.snake.vx === 0) { // left
+                    this.snake.vx = -this.blockSize;
+                    this.snake.vy = 0;
+                } else if (event.which === 87 && this.snake.vy === 0) { // up
+                    this.snake.vx = 0;
+                    this.snake.vy = -this.blockSize;
+                } else if (event.which === 68 && this.snake.vx === 0) { // right
+                    this.snake.vx = this.blockSize;
+                    this.snake.vy = 0;
+                } else if (event.which === 83 && this.snake.vy === 0) { // down
+                    this.snake.vx = 0;
+                    this.snake.vy = this.blockSize;
+                }
             }
+
         });
     }
 
-    controlSecondSnake() {
-        document.addEventListener('keydown', (event) => {
-            if (event.which === 65 && this.secondSnake.vx === 0) { // left
-                this.secondSnake.vx = -this.blockSize;
-                this.secondSnake.vy = 0;
-            } else if (event.which === 87 && this.secondSnake.vy === 0) { // up
-                this.secondSnake.vx = 0;
-                this.secondSnake.vy = -this.blockSize;
-            } else if (event.which === 68 && this.secondSnake.vx === 0) { // right
-                this.secondSnake.vx = this.blockSize;
-                this.secondSnake.vy = 0;
-            } else if (event.which === 83 && this.secondSnake.vy === 0) { // down
-                this.secondSnake.vx = 0;
-                this.secondSnake.vy = this.blockSize;
-            }
-        });
-    }
 
-    drawSnake(snake) {
-        snake.x += snake.vx;
-        snake.y += snake.vy;
+
+    drawSnake() {
+        this.snake.x += this.snake.vx;
+        this.snake.y += this.snake.vy;
     
-        this.setWallCollision(snake);
-        this.changeTailLength(snake);
-        this.colorizeSnake(snake);
-        this.checkFoodCollision(snake);
-        this.restartCondition(snake);
+        this.setWallCollision();
+        this.changeTailLength();
+        this.colorizeSnake();
+        this.checkFoodCollision();
+        this.restartCondition();
     }
 
-    changeTailLength(snake) {
-        snake.tail.unshift({ x: snake.x, y: snake.y});
+    changeTailLength() {
+        this.snake.tail.unshift({ x: this.snake.x, y: this.snake.y});
         
-        if (snake.tail.length > snake.tailLength) {
-            snake.tail.pop();
+        if (this.snake.tail.length > this.snake.tailLength) {
+            this.snake.tail.pop();
         }
     }
 
-    restartCondition(snake) {
-        snake.tail.forEach((elem,index) => {
+    restartCondition() {
+        this.snake.tail.forEach((elem,index) => {
     
-            for (let i = index + 1; i < snake.tail.length; i++) {
-                if (elem.x === snake.tail[i].x && elem.y === snake.tail[i].y) {
-                    this.restart(snake);
+            for (let i = index + 1; i < this.snake.tail.length; i++) {
+                if (elem.x === this.snake.tail[i].x && elem.y === this.snake.tail[i].y) {
+                    this.restart();
                 }
             }
         });
     }
 
-    checkFoodCollision(snake) {
-        snake.tail.forEach((elem) => {
+    checkFoodCollision() {
+        this.snake.tail.forEach((elem) => {
     
             if (elem.x === this.food.x && elem.y === this.food.y) {
-                snake.tailLength++;
+                this.snake.tailLength++;
                 this.incrScore();
                 this.setRandomFoodCoords();
             }
         });
     }
 
-    colorizeSnake(snake) {
-        snake.tail.forEach((elem,index) => {
+    colorizeSnake() {
+        this.snake.tail.forEach((elem,index) => {
             if (index === 0) {
                 this.context.fillStyle = '#000000';
-            } else if (snake === this.snake){
-                this.context.fillStyle = 'red';
             } else {
-                this.context.fillStyle = 'blue';
+                this.context.fillStyle = 'red';
             }
             this.context.fillRect(elem.x, elem.y, this.blockSize, this.blockSize);
         });
@@ -157,15 +147,15 @@ class Game {
         this.context.fillRect(this.width - this.blockSize, this.blockSize * 4, this.blockSize, this.height);
     }
 
-    setWallCollision(snake) {
-        if(snake.x < this.blockSize) {
-            snake.x = this.width - this.blockSize * 2;
-        } else if(snake.x >= this.width - this.blockSize) {
-            snake.x = this.blockSize;
-        } else if(snake.y < this.blockSize * 5) {
-            snake.y = this.height - this.blockSize * 2;
-        } else if(snake.y >= this.height - this.blockSize) {
-            snake.y = this.blockSize * 5;
+    setWallCollision() {
+        if(this.snake.x < this.blockSize) {
+            this.snake.x = this.width - this.blockSize * 2;
+        } else if(this.snake.x >= this.width - this.blockSize) {
+            this.snake.x = this.blockSize;
+        } else if(this.snake.y < this.blockSize * 5) {
+            this.snake.y = this.height - this.blockSize * 2;
+        } else if(this.snake.y >= this.height - this.blockSize) {
+            this.snake.y = this.blockSize * 5;
         }
     }
 
@@ -179,14 +169,14 @@ class Game {
         this.food.y = this.randomizer(5, this.blockHeight - 1) * this.blockSize;
     }
 
-    restart(snake) {
-        score = 0;
-        snake.x = this.width / 2;
-        snake.y = this.height / 2;
-        snake.vx = this.blockSize;
-        snake.vy = 0;
-        snake.tail = [];
-        snake.tailLength = 3;
+    restart() {
+        this.score = 0;
+        this.snake.x = this.width / 2;
+        this.snake.y = this.height / 2;
+        this.snake.vx = this.blockSize;
+        this.snake.vy = 0;
+        this.snake.tail = [];
+        this.snake.tailLength = 3;
     }
 
     drawScore() {
@@ -211,7 +201,13 @@ class Game {
     
 }
 
-let game = new Game();
-
+let game = new Game('.gamefield');
 game.gameLoop();
+console.log(game);
+let anotherGame = new Game('.anotherGamefield');
+anotherGame.gameLoop();
+console.log(anotherGame);
+
+
+
 
